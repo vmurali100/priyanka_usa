48feaab811d75c2dbcc200417960066bb05b135d
vmurali100 <vmurali100@gmail.com>
2020-11-26 07:32:57 +0530

    Adding Code

---
+++ Objects/sample2_objects.ts
@@ -0,0 +1,7 @@
+function Person(fname: String, lname: String, age: Number) {
+  this.fname = fname;
+  this.lname = lname;
+  this.age = age;
+}
+
+var person1 = new Person("Murali", "krishna", "abd");
